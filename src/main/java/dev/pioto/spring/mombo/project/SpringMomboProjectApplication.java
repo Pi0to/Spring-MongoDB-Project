77a0281ad08c9dd1a017bace5042d3e10154f441
Théo Pioto <theopioto@gmail.com>
2025-02-21 15:11:24 -0300

    first commit

---
+++ src/main/java/dev/pioto/spring/mombo/project/SpringMomboProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.pioto.spring.mombo.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMomboProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMomboProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pioto/spring/mombo/project/SpringMomboProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pioto.spring.mombo.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMomboProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
