a5898187e2c9d736675d1519530b08be3e116899
Théo Pioto <theopioto@gmail.com>
2025-02-21 15:56:15 -0300

    user entity and controller

---
+++ src/main/java/dev/pioto/spring/mombo/project/models/entities/User.java
@@ -0,0 +1,57 @@
+package dev.pioto.spring.mombo.project.models.entities;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private String userId;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(userId);
+    }
+}
